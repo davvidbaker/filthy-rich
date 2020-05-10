eabfcee605ee542c467a5431eb7445fda0516bdc
david.baker <david.baker@workday.com>
2020-05-10 12:03:01 -0500

    catch broken doc

--- src/index.js
+++ src/index.js
@@ -11,15 +11,37 @@ import './styles/editor-scope.css'
 
 const emptyDoc = schema.nodes.doc.createAndFill().toJSON()
 
-export function filthy(node, doc = emptyDoc, onChange = (_state) => {}) {
-  return new EditorView(node, {
-    state: EditorState.fromJSON(
+function getState(doc) {
+  try {
+    return EditorState.fromJSON(
       {
         schema,
         plugins: buildPlugins(),
       },
       { doc, selection: { type: 'text', anchor: 1, head: 1 } },
-    ),
+    )
+  } catch (e) {
+    console.warn(
+      `Could not get editor state from provided doc instantiating with empty doc
+    
+    Error: `,
+      e,
+    )
+    return EditorState.fromJSON(
+      {
+        schema,
+        plugins: buildPlugins(),
+      },
+      { doc: emptyDoc, selection: { type: 'text', anchor: 1, head: 1 } },
+    )
+  }
+}
+
+export function filthy(domNode, doc = 'emptyDoc', onChange = (_state) => {}) {
+  const state = getState(doc)
+
+  return new EditorView(domNode, {
+    state,
     dispatchTransaction(tr) {
       this.updateState(this.state.apply(tr))
 
